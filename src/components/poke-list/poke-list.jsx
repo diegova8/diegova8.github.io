5be5801433321680ef68cd10c883733022d4a2da
diegova8 <dvargas.dev@gmail.com>
2023-06-12 14:59:06 -0600

    Pokelist refactor to use id from params

--- src/components/poke-list/poke-list.jsx
+++ src/components/poke-list/poke-list.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { pokeball, black } from "../../assets";
 
 const PokeList = ({ pokemon }) => {
-  const [active, setActive] = useState(null);
+  const { pokemonId } = useParams();
+
   return (
     <div className="flex flex-col-reverse h-screen w-full">
       <div>
@@ -14,23 +14,17 @@ const PokeList = ({ pokemon }) => {
         <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
           {pokemon &&
             pokemon.map((pokemon) => (
-              <Link
-                to={`pokemon/${pokemon.name}`}
-                key={pokemon.name}
-                onClick={() => {
-                  setActive(pokemon.name);
-                }}
-              >
+              <Link to={`pokemon/${pokemon.name}`} key={pokemon.name} s>
                 <li
                   key={pokemon.name}
                   className={`${
-                    active === pokemon.name ? "bg-red-500" : ""
+                    pokemonId === pokemon.name ? "bg-red-500" : ""
                   } m-1 p-2 sm:p-3 sm:m-2 hover:bg-red-500 rounded`}
                 >
                   <div className="flex items-center space-x-4">
                     <div className="flex flex-row">
                       <img
-                        src={active === pokemon.name ? pokeball : black}
+                        src={pokemonId === pokemon.name ? pokeball : black}
                         alt={pokemon.name}
                         className="w-5 h-5 mx-2 bg-gray-300 rounded-full flex-shrink-0"
                       ></img>
